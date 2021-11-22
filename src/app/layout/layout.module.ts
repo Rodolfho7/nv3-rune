7f943bd558ad725d6c15fb8991c0a17c7fecb5e5
unknown <rodolfhoazevedo@gmail.com>
2021-11-21 21:13:50 -0300

    add footer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,18 +3,21 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { Nv3LogoComponent } from './header/nv3-logo/nv3-logo.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    Nv3LogoComponent
+    Nv3LogoComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class LayoutModule { }
